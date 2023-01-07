f0d0c19052be2ce9c86cbbba9345e5018a91336f
paulceejay <paulchinedu717@gmail.com>
2023-01-07 07:27:55 +0100

    first commit

---
+++ pages/events/[...slug].jsx
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+    return(
+        <div>
+            <h2>ghrug</h2>
+        </div>
+    )
+}
+
+export default FilteredEventsPage
---
+++ pages/events/[eventsId].jsx
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+    return(
+        <div>
+            <h1>Events details page</h1>
+        </div>
+    )
+}
+
+export default EventDetailPage
